6d51c9d4f9ad0c62dd209fe2e87e613906d1558b
Salim Kachemela <sakachemela@gmail.com>
2022-07-19 18:06:17 +0300

    Finish with docker configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './modules/user/user.controller';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
